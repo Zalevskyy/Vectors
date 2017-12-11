c28cb36b31274ee18e94f55a2d295450dbe970a3
Ігор Залевський < >
2017-12-11 04:08:39 +0200

    first edition

---
+++ Epam_Vector/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam_Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new Vector(1, 2, 5);
+            Vector vector2 = new Vector(4, 8, 1);
+            //Add two vectors
+            Console.WriteLine("Додавання(перегрузка): " + vector1 + " + " + vector2 + " = " + (vector1 + vector2));
+            //another way add
+            //Vector vectorRes = vector1.Add(vector2);
+            Console.WriteLine("Додавання(метод): " + vector1 + " + " + vector2 + " = " + vector1.Add(vector2));
+            //Substracting two vectors
+            Console.WriteLine("Віднімання(перегрузка): " + vector1 + " - " + vector2 + " = " + (vector1 - vector2));
+            //another way substracting
+            Console.WriteLine("Віднімання(метод): " + vector1 + " - " + vector2 + " = " + vector1.Subs(vector2));
+            //Scalar multiplying two vectors
+            vector1 = new Vector(1, 2, -5);
+            vector2 = new Vector(4, 8, 1);
+            Console.WriteLine("Скалярний добуток(метод): " + vector1 + " scalar* " + vector2 + " = " + vector1.MultScalar(vector2));
+            //Vector multiplying two vectors
+            vector1 = new Vector(-1, 2, -2);
+            vector2 = new Vector(2, 1, -1);
+            Console.WriteLine("Векторний добуток(метод): " + vector1 + " * " + vector2 + " = " + vector1.MultVector(vector2));
+            //another way multiplying
+            vector1 = new Vector(1, 2, 3);
+            vector2 = new Vector(2, 1, -2);
+            Console.WriteLine("Векторний добуток(перегрузка): " + vector1 + " * " + vector2 + " = " + (vector1 * vector2));
+            //Mixed multiplying two vectors
+            vector1 = new Vector(1, 2, 3);
+            vector2 = new Vector(1, -1, 1);
+            Vector vector3 = new Vector(2, 0, -1);
+            Console.WriteLine("Мішаний добуток векторів: {0}, {1}, {2} = {3}", vector1, vector2, vector3, vector1.MultMixed(vector2, vector3));
+            //Angle between two vectors
+            vector1 = new Vector(1, 0, 3);
+            vector2 = new Vector(5, 5, 0);
+            double cosA = vector1.CosAngle(vector2);
+            Console.WriteLine("Косинус кута між вектором {0} і {1} = {2}",vector1, vector2, vector1.CosAngle(vector2));
+            //Comparison of two vectors
+            Console.WriteLine("Порівняння "+vector1+" з "+vector2+" = "+vector1.isEqual(vector2));
+            vector1 = new Vector(5, 5, 0);
+            Console.WriteLine("Порівняння " + vector1 + " з " + vector2 + " = " + vector1.isEqual(vector2));
+        }
+    }
+}
---
+++ Epam_Vector/Vector.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam_Vector
+{
+    /// <summary>
+    /// Class represents a vector
+    /// </summary>
+    class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        /// <summary>
+        /// constructor for vector
+        /// </summary>
+        /// <param name="x">x coordinate</param>
+        /// <param name="y">y coordinate</param>
+        /// <param name="z">z coordinate</param>
+        public Vector (double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        /// <summary>
+        /// Overrided method ToString() for vector
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            try
+            {
+                return "{ " + X + ", " + Y + ", " + Z +" }";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        /// <summary>
+        /// method for calculating length 
+        /// </summary>
+        /// <returns></returns>
+        public double Lenght()
+        {
+            try
+            {
+                return Math.Sqrt(X * X + Y * Y + Z * Z);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Add two vectors
+        /// </summary>
+        /// <param name="v">vector</param>
+        /// <returns></returns>
+        public Vector Add (Vector v)
+        {
+            try
+            {
+                return new Vector(X + v.X, Y + v.Y, Z + v.Z);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        /// <summary>
+        /// Overloaded operator +
+        /// </summary>
+        /// <param name="v1">vector</param>
+        /// <param name="v2">vector</param>
+        /// <returns></returns>
+        public static Vector operator + (Vector v1, Vector v2)
+        {
+            try
+            {
+                return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        /// <summary>
+        /// Substract two vectors
+        /// </summary>
+        /// <param name="v">vector</param>
+        /// <returns></returns>
+        public Vector Subs(Vector v)
+        {
+            try
+            {
+                return new Vector(X - v.X, Y - v.Y, Z - v.Z);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        /// <summary>
+        /// Overloaded operator -
+        /// </summary>
+        /// <param name="v1">vector</param>
+        /// <param name="v2">vector</param>
+        /// <returns></returns>
+        public static Vector operator - (Vector v1, Vector v2)
+        {
+            try
+            {
+                return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        /// <summary>
+        /// Scalar multiplication
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public double MultScalar(Vector v)
+        {
+            try
+            {
+                return (X * v.X + Y * v.Y + Z * v.Z);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Vector multiplication
+        /// </summary>
+        /// <param name="v">vector</param>
+        /// <returns></returns>
+        public Vector MultVector(Vector v)
+        {
+            try
+            {
+                return new Vector(Y * v.Z - Z * v.Y, Z * v.X - X * v.Z, X * v.Y - Y * v.X);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        /// <summary>
+        /// Overloaded operator *
+        /// </summary>
+        /// <param name="v1">vector</param>
+        /// <param name="v2">vector</param>
+        /// <returns></returns>
+        public static Vector operator *(Vector v1, Vector v2)
+        {
+            try
+            {
+                return new Vector(v1.Y * v2.Z - v1.Z * v2.Y, v1.Z * v2.X - v1.X * v2.Z, v1.X * v2.Y - v1.Y * v2.X);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        /// <summary>
+        /// Mixed multiplication
+        /// </summary>
+        /// <param name="v1">vector</param>
+        /// <param name="v2">vector</param>
+        /// <returns></returns>
+        public double MultMixed(Vector v1, Vector v2)
+        {
+            try
+            {
+                return this.MultScalar(v1 * v2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Cosinus of the angle between two vectors
+        /// </summary>
+        /// <param name="v">vector</param>
+        /// <returns></returns>
+        public double CosAngle(Vector v)
+        {
+            try
+            {
+                return this.MultScalar(v) / (this.Lenght() * v.Lenght());
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divide by zero");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Comparison of two vectors
+        /// </summary>
+        /// <param name="v">vector</param>
+        /// <returns></returns>
+        public bool isEqual(Vector v)
+        {
+            try
+            {
+                return X == v.X && Y == v.Y;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
